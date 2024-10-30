ba829c47421e68cc883d6056a0b271c2a837e7a4
rahmanifahama <rahmanifahama102@gmail.com>
2024-10-30 11:22:11 -0400

    git push

--- Personal_Injury/Apps/Screens/NavigatorTabs/MoreSettings.jsx
+++ Personal_Injury/Apps/Screens/NavigatorTabs/MoreSettings.jsx
@@ -1,10 +1,1 @@
-import { View, Text } from "react-native";
-import React from "react";
 
-export default function MoreSettings() {
-  return (
-    <View>
-      <Text>MoreSettings</Text>
-    </View>
-  );
-}
